9a8fb04f46b51a8439139b6442bb900a90f652a2
Jed Duffey <jedduffey@gmail.com>
2023-03-28 15:46:42 -0400

    WIP

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react';
+import react from '@testing-library/react'
 
 export default defineConfig({
   resolve: {
